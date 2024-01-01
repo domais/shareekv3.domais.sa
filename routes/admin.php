eef6bde721e168b96b77417242e7df6a88b37f5d
rahmani djamel <hxhdono11@gmail.com>
2024-01-01 23:18:42 +0100

    Refactor routes and views, add PermitForm class

--- routes/admin.php
+++ routes/admin.php
@@ -8,6 +8,18 @@ Route::namespace('App\Livewire\Backend')->middleware('auth')->group(function() {
     Route::namespace('Permit')->prefix('permit')->as('permit.')->group(function() {
        
         Route::get('/', Index::class)->name('index');
+
+        Route::get('/create', Inputs::class)->name('create');
+    });
+
+    Route::namespace('Event')->prefix('event')->as('event.')->group(function() {
+       
+        Route::get('/', Index::class)->name('index');
+    });
+
+    Route::namespace('Show')->prefix('{type}')->as('show.')->group(function() {
+       
+        Route::get('/{id}', Index::class)->name('index');
     });
 
 });
