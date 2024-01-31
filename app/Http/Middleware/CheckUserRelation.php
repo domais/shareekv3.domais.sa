fd57407bd4d293fc7b740e6a84b169e4af9f19cd
rahmani djamel <hxhdono11@gmail.com>
2024-01-31 17:26:13 +0100

    Fix typo in CheckUserRelation middleware

--- app/Http/Middleware/CheckUserRelation.php
+++ app/Http/Middleware/CheckUserRelation.php
@@ -20,7 +20,7 @@ class CheckUserRelation
         $data = $request->route('permit');
         $user = $request->user();
 
-            if ($data->user_id != $user->id && $user->roles()->hasRoles('User')){
+            if ($data->user_id != $user->id && $user->roles()->hasRole('User')){
                 // Handle the case where the user is not the owner of the event
                 abort(403, 'عملية غير مصرح بها');
             }
