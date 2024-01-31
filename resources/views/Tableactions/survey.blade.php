cabf2e5f07b61f1b5c6a28a7c02b5f72c2078fc0
Mohd Domais <md@toot.sa>
2024-01-31 10:32:42 +0300

    Update survey.blade.php

--- resources/views/Tableactions/survey.blade.php
+++ resources/views/Tableactions/survey.blade.php
@@ -1,9 +1,8 @@
 <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-    <div class="btn-group mx-2" role="group" aria-label="Third group">
-        <a role="button"  href="{{route('survey.show',$row->id)}}">
-          <i class="fa-solid fa-eye btn btn-outline-light"></i>
-        </a>
-    </div>
-    </div>
-  </div>
-
+	<div class="btn-group mx-2" role="group" aria-label="Third group">
+		<a role="button" href="{{ route('survey.show', $row->id) }}">
+			<x-heroicon-o-eye style="width: 28px" />
+		</a>
+	</div>
+</div>
+</div>
