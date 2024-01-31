b0b1f7870eaad53b80d5e62555aaca0008eb36a2
rahmani djamel <hxhdono11@gmail.com>
2024-01-31 21:25:18 +0100

    Add debug statement to display permit histories

--- app/Livewire/Backend/Permit/Inputs.php
+++ app/Livewire/Backend/Permit/Inputs.php
@@ -75,6 +75,8 @@ class Inputs extends Component
 
         if ($this->is_show_page && $this->permit) {
             $this->histories = $this->permit->history()->orderBy('created_at', 'asc')->get();
+
+            dd($this->histories);
         }
         if (is_null($this->permit)) {
             $this->form->lat = auth()->user()->owner->lat;
