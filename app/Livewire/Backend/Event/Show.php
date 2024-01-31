f9d8e4b564a335768cf1e8970197dd7a3bcc12f0
rahmani djamel <hxhdono11@gmail.com>
2024-01-31 21:38:19 +0100

    Remove debug statement from Show.php

--- app/Livewire/Backend/Event/Show.php
+++ app/Livewire/Backend/Event/Show.php
@@ -55,8 +55,6 @@ class Show extends Component
 
         if ($this->is_show_page && $this->permit) {
             $this->histories = $this->permit->history()->orderBy('created_at', 'asc')->get()->reverse();
-            dd($this->histories);
-
         }
         if (is_null($this->permit)) {
             $this->form->lat = auth()->user()->owner->lat;
