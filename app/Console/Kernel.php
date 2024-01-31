9ffd3d33371a07af60d261a71232d896fde96d71
rahmani djamel <hxhdono11@gmail.com>
2024-01-31 16:03:09 +0100

    Disable sending event command every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         // $schedule->command('app:survey')->dailyAt('12:00');
         $schedule->command('app:survey')->hourly();
-        $schedule->command('send:event')->everyMinute();
+        //$schedule->command('send:event')->everyMinute();
 
     }
 
