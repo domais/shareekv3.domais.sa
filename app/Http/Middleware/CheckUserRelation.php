13180cfe702afae9d6a0021673630aa72b10e35c
Mohd Domais <md@toot.sa>
2024-02-01 10:22:14 +0300

    Update CheckUserRelation.php

--- app/Http/Middleware/CheckUserRelation.php
+++ app/Http/Middleware/CheckUserRelation.php
@@ -20,8 +20,8 @@ class CheckUserRelation
         $data = $request->route('order_number');
         $data = Permit::where('order_number', $data)->first();
         $user = $request->user();
-            if ($data->user_id != $user->id && $user->hasRole('User')){
-                // Handle the case where the user is not the owner of the event
+            if (isset($data->user_id) && $user->hasRole('User') && $data->user_id != $user->id){
+                // Handle the case where the user is not the owner of the event // Modefied by Domais
                 abort(403, 'عملية غير مصرح بها');
             }
 
