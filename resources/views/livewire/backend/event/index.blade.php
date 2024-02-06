8b4a467869f7a3905681b7491d9270f36a309b25
rahmani djamel <hxhdono11@gmail.com>
2024-02-06 21:55:13 +0100

    Refactor event index.blade.php file

--- resources/views/livewire/backend/event/index.blade.php
+++ resources/views/livewire/backend/event/index.blade.php
@@ -152,17 +152,17 @@ x-data="{errors: @entangle('ValidationErrors').live,
                             </div>                        
                         </div>
                     </div>
-                    <div x-show="selected_event && selected_event.event_location != 3" x-data="{ checked: false }" >
+                    <div x-show="selected_event && selected_event.event_location != 3" class="my-3" x-data="{ checked: false }" >
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" @click="checked = !checked">
                             <label class="form-check-label" for="flexCheckDefault">
                                 هل كان هناك بث مباشر للفعالية؟ 
                             </label>
                         </div>
-                    
-                        <div x-show="checked">
-                            <label for="inputField" class="form-label">عدد الحضور</label>
-                            <input type="text" class="form-control" id="inputField">
+
+                        <div class="mt-3 row mx-0" x-show="checked">
+                            <div class="col-3 px-0 d-flex align-items-center"> عدد الحضور : </div>
+                            <div class="col-8"><input type="text" class="form-control" id="inputField"></div>
                         </div>
                     </div>
         
