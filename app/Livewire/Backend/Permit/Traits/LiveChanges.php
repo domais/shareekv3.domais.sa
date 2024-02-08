79377498fd3744095a3aa8bd6ad928938d76f19c
rahmani djamel <hxhdono11@gmail.com>
2024-02-08 19:47:22 +0100

    Fix condition for checking current route name

--- app/Livewire/Backend/Permit/Traits/LiveChanges.php
+++ app/Livewire/Backend/Permit/Traits/LiveChanges.php
@@ -218,13 +218,13 @@ trait LiveChanges
 
                     $permit->update($permitData);
                 }
-                if(!Route::currentRouteNamed('permit.edit')) {
+                if(!Route::currentRouteName() == 'permit.edit') {
                     $permit->user->owner->points = $permit->user->owner->points - $counter_speakers;
+
                     if ($permit->user->owner->points < 0) {
                         throw new \Exception('عفواً .. لقد استهلكت كامل رصيدك للدعم اللوجستي');
                     }
                 }
-
                 $permit->user->owner->save();
                 $permit->points = $counter_speakers;
                 $permit->save();
