56c50c9445d9cd109f8ecc609482ec5920a54527
rahmani djamel <hxhdono11@gmail.com>
2024-02-09 01:51:37 +0100

    Add HorizonServiceProvider to app.php

--- config/app.php
+++ config/app.php
@@ -171,6 +171,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\VoltServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
+
         
     ])->toArray(),
 
