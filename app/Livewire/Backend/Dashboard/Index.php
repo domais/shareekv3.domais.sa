a6fe4ea0f44e9e7835d5af51cdbfccc6e271f4c2
rahmani djamel <hxhdono11@gmail.com>
2024-02-11 18:22:26 +0100

    Add Partner model and retrieve top 10 partners with permit count

--- app/Livewire/Backend/Dashboard/Index.php
+++ app/Livewire/Backend/Dashboard/Index.php
@@ -3,16 +3,25 @@
 namespace App\Livewire\Backend\Dashboard;
 
 use App\Models\Event;
+use App\Models\Partner;
 use Livewire\Component;
 
 class Index extends Component
 {
     public $event_counter;
     public $chart_one = [];
+    public $partners = [];
+
     public function mount()
     {
         $this->event_counter = Event::count();
+
+        $this->partners = Partner::withCount('owner.permits as permits_count')
+        ->orderBy('permits_count', 'desc')
+        ->take(10)
+        ->get();
         
+        dd($this->partners);
     }
     public function render()
     {
