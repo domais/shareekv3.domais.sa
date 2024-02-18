cd74dfff06269ac1a2d518aadfa1c56816dc8a61
rahmani djamel <hxhdono11@gmail.com>
2024-02-18 15:13:50 +0100

    Add verification code functionality to forgot password form

--- resources/views/livewire/pages/auth/forgot-password.blade.php
+++ resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -16,9 +16,34 @@ new #[Layout('layouts.auth')] class extends Component
 {
     public $errors = [];
     public $email = '';
+    public $step_two = false;
+    public = $code = '';
+
+    public function verifyCode()
+    {
+        $user = User::where('email', $this->email)->first();
+
+        $otp = Code::where('user_id', $user->id)
+                    ->where('code', $this->code)
+                    ->where('created_at', '>=', now()->subMinutes(5))
+                    ->first();
+
+        if ($otp) {
+            dd('verified');
+           
+        } else {
+            $this->errors = ['الرمز المرة الذي أدخلته غير صالح أو قد انتهى.'];
+         return ;
+        }
+    }
 
     public function login()
     {
+        if ($this->step_two) {
+            # code...
+            $this->verifyCode();
+            return ;
+        }
         try {
             // Validate the email field
             $validatedData = $this->validate([
@@ -38,6 +63,7 @@ new #[Layout('layouts.auth')] class extends Component
             if ($otpCount < 3) {
                 // If less than 3 OTP codes have been generated today, generate a new one
                 sendOtp($user);
+                $this->step_two = true;
             } else {
                 // Otherwise, return an error message
                 $this->ValidationErrors = ['لقد وصلت إلى الحد الأقصى لعدد طلبات  لهذا اليوم. ارجوك حاول مرة أخرى غدا.'];
@@ -76,10 +102,24 @@ x-data="{errors: @entangle('errors').live}"
 	<form wire:submit="login">
 		<h3 class="fs-3 fw-bold text-center">ضبط كلمة السر</h3>
 
-		<input placeholder="البريد الإلكتروني" wire:model="email" autocomplete="email" type="email" class="form-control my-3 text-center">		
+        @if ($step_two)
+
+        <input placeholder="رمز التحقق" wire:model="code" autocomplete="code" type="number" class="form-control my-3 text-center">		
+
+            
+        @else
+
+        <input placeholder="البريد الإلكتروني" wire:model="email" autocomplete="email" type="email" class="form-control my-3 text-center">		
+
+            
+        @endif
 
 		<button type="submit" class="btn btn-brand mt-4">
-			ارسل رمز التحقق
+            @if($step_two)
+                تأكيد الرمز
+            @else
+                ارسل رمز التحقق
+            @endif
 		</button>
 	</form>
 	<div class="border-top pt-4 mt-4 d-flex justify-content-between">
