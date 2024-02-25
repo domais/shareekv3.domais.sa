29686c4b45d576eebf0e23c9e9a31e2f9c96d76a
rahmani djamel <hxhdono11@gmail.com>
2024-02-25 21:30:55 +0100

    Refactor store method in Show.php

--- app/Livewire/Backend/Draft/Show.php
+++ app/Livewire/Backend/Draft/Show.php
@@ -62,7 +62,7 @@ class Show extends Component
 
     public function store($status)
     {
-       dd($status);
+       
         $this->form->user_id = auth()->user()->id;
 
         try {
@@ -76,21 +76,6 @@ class Show extends Component
                 $startDate = Carbon::parse($this->form->start_date);
                 $now = Carbon::now();
                 $difference = $now->diffInDays($startDate, false);
-                /*
-                if ($difference < 3) {
-                    $validator = Validator::make([], []); // empty data and rules
-                    $validator->errors()->add('start_date', 'يجب أن يكون تاريخ البداية بعد خمسة أيام على الأقل من الآن');
-                    throw new ValidationException($validator);
-                }
-                  
-                 later */
-
-                 /*
-                if (auth()->user()->hasRole('User') && auth()->user()->owner->points == 0) {
-                    $validator = Validator::make([], []); // empty data and rules
-                    $validator->errors()->add('points', 'ليس لديك رصيد كافي للدعم اللوجستي');
-                    throw new ValidationException($validator);
-                }*/
 
             }
         } catch (ValidationException $th) {
@@ -99,27 +84,12 @@ class Show extends Component
         }
         $permitData = $this->form->toArray();
 
-        if ($status == 1) {
-            if (empty($this->form->litrary_children_id)  || empty($this->form->title) ) {
-                $this->errors[] = "يجب إدخال العنوان و التصنيف الأدبي" ; 
-                return;
-            }
-            $this->saveDraft($permitData, $this->draft,$this->speakers,$this->partnerships);
-        }
-        else{
-
-            
             try {
                 $this->savePermit($permitData, $this->speakers, $this->partnerships, $this->permit);
             } catch (\Exception $e) {
                 $this->errors = [$e->getMessage()];
                 return;
             }            
-        }
-        if ($this->draft) {
-            session(['draft_to_delete' => $this->draft->id]);
-        }
-
         $this->dispatch('DeletePermit_Response', array_merge(SwalResponse(), ['place' => 'inside']));
 
     }
