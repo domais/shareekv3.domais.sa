d0b8817f788fa46eebe4a9b2fcbc7d7bb69a3f0f
rahmani djamel <hxhdono11@gmail.com>
2024-02-25 21:26:55 +0100

    Add store method to Show component

--- app/Livewire/Backend/Draft/Show.php
+++ app/Livewire/Backend/Draft/Show.php
@@ -8,6 +8,8 @@ use App\Livewire\Forms\PermitForm;
 use App\Livewire\Forms\SpeakerForm;
 use App\Models\Draft;
 use App\Models\Permit;
+use Carbon\Carbon;
+use Illuminate\Validation\ValidationException;
 use Livewire\Component;
 
 class Show extends Component
@@ -55,9 +57,72 @@ class Show extends Component
         
         if ($this->permit) {
             $this->form->setFormDraft($this->permit);
-        }   
-
+        }  
     }   
+
+    public function store($status)
+    {
+       dd($status);
+        $this->form->user_id = auth()->user()->id;
+
+        try {
+            $this->form->store($status);
+            if ($status == 2 && empty($this->speakers) && $this->form->event_type_id == 1) {
+                $this->errors[] = "يجب إضافة متحدث واحد على الأقل" ;
+                return;
+            }
+         
+            if ($status == 2) {
+                $startDate = Carbon::parse($this->form->start_date);
+                $now = Carbon::now();
+                $difference = $now->diffInDays($startDate, false);
+                /*
+                if ($difference < 3) {
+                    $validator = Validator::make([], []); // empty data and rules
+                    $validator->errors()->add('start_date', 'يجب أن يكون تاريخ البداية بعد خمسة أيام على الأقل من الآن');
+                    throw new ValidationException($validator);
+                }
+                  
+                 later */
+
+                 /*
+                if (auth()->user()->hasRole('User') && auth()->user()->owner->points == 0) {
+                    $validator = Validator::make([], []); // empty data and rules
+                    $validator->errors()->add('points', 'ليس لديك رصيد كافي للدعم اللوجستي');
+                    throw new ValidationException($validator);
+                }*/
+
+            }
+        } catch (ValidationException $th) {
+            $this->errors = $th->validator->errors()->all();
+            return;
+        }
+        $permitData = $this->form->toArray();
+
+        if ($status == 1) {
+            if (empty($this->form->litrary_children_id)  || empty($this->form->title) ) {
+                $this->errors[] = "يجب إدخال العنوان و التصنيف الأدبي" ; 
+                return;
+            }
+            $this->saveDraft($permitData, $this->draft,$this->speakers,$this->partnerships);
+        }
+        else{
+
+            
+            try {
+                $this->savePermit($permitData, $this->speakers, $this->partnerships, $this->permit);
+            } catch (\Exception $e) {
+                $this->errors = [$e->getMessage()];
+                return;
+            }            
+        }
+        if ($this->draft) {
+            session(['draft_to_delete' => $this->draft->id]);
+        }
+
+        $this->dispatch('DeletePermit_Response', array_merge(SwalResponse(), ['place' => 'inside']));
+
+    }
     
     public function render()
     {
