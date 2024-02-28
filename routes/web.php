c0a1ba7b84455fde2b4fb1359a631351c0e6c423
rahmani djamel <hxhdono11@gmail.com>
2024-02-28 01:02:46 +0100

    Add quick login route

--- routes/web.php
+++ routes/web.php
@@ -171,7 +171,7 @@ Route::get('/delete-firebase/{token}', function ($token) {
     return 'Deleted all users, events, partners created from firebase';
 });
 
-
+/*
 Route::get('/quick-login/{email}', function ($email) {
     $user = User::where('email', $email)
         ->orWhere('id', $email)
@@ -182,3 +182,4 @@ Route::get('/quick-login/{email}', function ($email) {
     }
     return redirect()->route('login');
 });
+*/
